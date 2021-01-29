bee15622a4b2b40da75490f69260b8013981c522
Ardranger <daniel.cullen@ucdconnect.ie>
2021-01-29 23:15:26 +0000

    Moved data to single df. To do: Drop duplicates and sort by datetime col

--- synchronise_dbs.py
+++ synchronise_dbs.py
@@ -8,13 +8,29 @@ def sync_dbs():
     
     bash_subprocess = subprocess.run("scp -i ~/.ssh/ssh-key-2021-01-28.key opc@132.145.78.50:/home/opc/Data/coindesk.db /tmp/db_vm2", shell=True)
 
-    bash_subprocess = subprocess.run("scp -i ~/.ssh/ssh-key-2020-11-24.key opc@132.145.55.214:/home/opc/Data/coindesk.db /tmp/db_vm1", shell=True)
+
+def merge():
+    vm1_df = load_db_to_df("/tmp/db_vm1")
+    #print(vm1_df)
+    
+    vm2_df = load_db_to_df("/tmp/db_vm2")
+    #print(vm2_df)
+
+    rpi_df = load_db_to_df("/mnt/external_hdd/Data/coindesk.db")
+    #print(rpi_df)
+
+    uber_df = pd.concat([vm1_df,vm2_df,rpi_df])
+    print(uber_df)
 
 
-def load_db_to_df():
+def load_db_to_df(path_to_sqlite_db):
 
+    conn = sqlite3.connect(path_to_sqlite_db)
+    df = pd.read_sql_query("SELECT * FROM bitcoin_coinbase", conn)
+    conn.close()
+    return df
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    sync_dbs()
+    #sync_dbs()
+    merge()
