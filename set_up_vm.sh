5b139196346966dbb6422a79123dfdb57d5e944a
Ardranger <daniel.cullen@ucdconnect.ie>
2022-01-10 22:17:29 +0000

    Added conda set up section for arm/x86

--- set_up_vm.sh
+++ set_up_vm.sh
@@ -1,4 +1,4 @@
-# For ubuntu lts
+## For ubuntu lts ##
 
 sudo apt-get update; sudo apt-get upgrade -y
 
@@ -13,3 +13,23 @@ sudo mount /mnt/external_hdd
 ln -s  /mnt/external_hdd ~/hdd
 
 gnome-tweak-tool
+
+
+# Conda set up 
+arch_type=$(dpkg --print-architecture)
+case $arch_type in
+        amd64)
+                wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+                ;;
+        armhf)
+                wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+                ;;
+esac
+
+chmod +x Miniconda3-latest*
+
+bash Miniconda-latest* -b
+
+## To recreate the python enviroment ##
+# conda create --name bitcoin_backtest --file bitcoin_backtest.txt
+# conda activate bitcoin_backtest
